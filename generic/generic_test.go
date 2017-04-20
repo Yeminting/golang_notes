66dd051678c1cd78f15150ceef694d42b920e276
Yeminting <yeminting_china@hotmail.com>
2017-04-21 07:00:48 +0800

    update generic

---
+++ generic/generic_test.go
@@ -0,0 +1,39 @@
+package generic
+
+import "testing"
+
+type person struct {
+	ID   string
+	Name string
+}
+
+//IsEquel 继承对别借口compare
+func (this person) IsEquel(other interface{}) bool {
+	if p, ok := other.(person); ok {
+		return this.ID == p.ID
+	}
+	return false
+}
+
+func TestGeneric(t *testing.T) {
+	list := List{}
+	p1 := person{"1", "zhangsan"}
+	p2 := person{"2", "lisi"}
+	p3 := person{"3", "wangwu"}
+	list.Add(p1)
+	list.Add(p2)
+	list.Add(p3)
+	t.Log(list)
+
+	p4 := person{"1", "zhangsan11"}
+	list.Remove(p4)
+	t.Log(list)
+
+	list.RemoveAll(func(a interface{}) bool {
+		if p, ok := a.(person); ok {
+			return p.ID == p2.ID
+		}
+		return false
+	})
+	t.Log(list)
+}
